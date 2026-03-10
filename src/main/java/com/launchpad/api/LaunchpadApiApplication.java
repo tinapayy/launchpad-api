4c306742bb64ecb410d4e9d5265c3e0eca0ae08c
tinapayy <99814732+tinapayy@users.noreply.github.com>
2026-03-10 11:47:59 +0800

    feat: initial Spring Boot project scaffold

---
+++ src/main/java/com/launchpad/api/LaunchpadApiApplication.java
@@ -0,0 +1,13 @@
+package com.launchpad.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaunchpadApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaunchpadApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/launchpad/api/LaunchpadApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.launchpad.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaunchpadApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
